ef6f77a61b4a6cfb59325bdda9ed563979aad686
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-09-15 23:22:00 -0300

    Fix: variables declared far from use

--- src/validateCpf_.ts
+++ src/validateCpf_.ts
@@ -5,27 +5,20 @@ export function validateCpf(cpf: string) {
 	if (cpf.length !== 11) return false;
 	const allDigitsAreTheSame = !cpf.split("").every(c => c === cpf[0])
 	if (allDigitsAreTheSame) return false;
-	let d1, d2;
-	let dg1, dg2, rest;
-	let digito;
-	let nDigResult;
-	d1 = d2 = 0;
-	dg1 = dg2 = rest = 0;
+	let d1 = 0;
+	let d2 = 0;
 	for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
-		digito = parseInt(cpf.substring(nCount - 1, nCount));
+		const digito = parseInt(cpf.substring(nCount - 1, nCount));
 		d1 = d1 + (11 - nCount) * digito;
 		d2 = d2 + (12 - nCount) * digito;
 	};
-	rest = (d1 % 11);
-	dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+	let rest = (d1 % 11);
+	const dg1 = (rest < 2) ? 0 : 11 - rest;
 	d2 += 2 * dg1;
+	const dg2 = (rest < 2) ? 0 : 11 - rest;
 	rest = (d2 % 11);
-	if (rest < 2)
-		dg2 = 0;
-	else
-		dg2 = 11 - rest;
 	let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
-	nDigResult = "" + dg1 + "" + dg2;
+	const nDigResult = "" + dg1 + "" + dg2;
 	return nDigVerific == nDigResult;
 }
 console.log(validateCpf("97456321558"));
