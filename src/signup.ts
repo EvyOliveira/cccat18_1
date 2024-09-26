84fdb6e9aeebdb2cab6750fe9f59d72e5e9a8cb7
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-09-25 22:08:08 -0300

    Refactor: reducing code complexity and rename variables code smells

--- src/signup.ts
+++ src/signup.ts
@@ -7,40 +7,36 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
-	const request = req.body;
-	const databaseConnection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const input = req.body;
+	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+	const id = crypto.randomUUID();
+	let result;
+	const [accountId] = await connection.query("select * from ccca.account where email = $1", [input.email]);
 
-	try {
-		const id = crypto.randomUUID();
-		let result;
-		const [accountId] = await databaseConnection.query("select * from ccca.account where email = $1", [request.email]);
+	if (accountId) return res.status(422).json({ message: -4 });
+	if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) return res.status(422).json({ message: -3 });
+	if (!input.email.match(/^(.+)@(.+)$/)) return res.status(422).json({ message: -2 });
+	if (!validateCpf(input.cpf)) return res.status(422).json({ message: -1 })
+	if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) return res.status(422).json({ message: -5 })
+	if (input.isDriver) {
+		if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+			await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
 
-		if (accountId) return result = -4;
-		if (!request.name.match(/[a-zA-Z] [a-zA-Z]+/)) return result = -3;
-		if (!request.email.match(/^(.+)@(.+)$/)) return result = -2;
-		if (!validateCpf(request.cpf)) return result = -1;
-		if (!request.carPlate.match(/[A-Z]{3}[0-9]{4}/)) return result = -5;
-		if (request.isDriver) {
-			await databaseConnection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, request.name, request.email, request.cpf, request.carPlate, !!request.isPassenger, !!request.isDriver, request.password]);
-			const response = {
+			const obj = {
 				accountId: id
 			};
-			result = response;
+			result = obj;
 		} else {
-			await databaseConnection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, request.name, request.email, request.cpf, request.carPlate, !!request.isPassenger, !!request.isDriver, request.password]);
-			const response = {
-				accountId: id
-			};
-			result = response;
+			result = -5;
 		}
-		if (typeof result === "number") {
-			res.status(422).json({ message: result });
-		} else {
-			res.json(result);
-		}
-	} finally {
-		await databaseConnection.$pool.end();
+	} else {
+		await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
+		const obj = {
+			accountId: id
+		};
+		result = obj;
 	}
+	if (typeof result === "number") { res.status(422).json({ message: result }); } else { res.json(result); }
 });
 
 app.listen(3000);
